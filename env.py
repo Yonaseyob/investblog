ceda48f23b99010c9abf87742dec00417e1171d0
Yonaseyob <112119971+Yonaseyob@users.noreply.github.com>
2023-06-19 11:49:48 +0200

    Create env.py
    Add env file

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://odzelfrm:9NH0mcA_zb0Ac48CHHcgQGp8ISmZ7Nij@snuffleupagus.db.elephantsql.com/odzelfrm"
+os.environ["SECRET_KEY"] = "changegc@2009"
+os.environ["CLOUDINARY_URL"] = "cloudinary://638216569397896:c3JZQeXjR93lEAEyrM0X0-Z1LWs@dhjgfeflg"
