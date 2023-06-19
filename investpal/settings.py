496dae9cd1779cac4453d6cb4bef381cc88ed5db
Yonaseyob <yonaseyobberhane@gmail.com>
2023-06-19 14:34:18 +0000

    Add allowed hosts

--- investpal/settings.py
+++ investpal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-yonaseyob-investblog-dv6j46jufpd.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-yonaseyob-investblog-t0k3nu9tpz9.ws-eu100.gitpod.io', 'investpal.herokuapp.com', 'localhost']
 
 
 # Application definition
